4e1172927799e83230e2c390d6d6a3edbf390c24
Laurent Kazus <lkazus@gmail.com>
2012-04-03 19:45:34 +0200

    remove stored memory logs

--- Log/LoggerWatchdog.php
+++ Log/LoggerWatchdog.php
@@ -12,12 +12,11 @@
 namespace Ekino\Bundle\DrupalBundle\Log;
 
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
-use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 
 /**
  * @author Laurent Kazus <kazus@ekino.com>
  */
-class LoggerWatchdog implements LoggerInterface, DebugLoggerInterface
+class LoggerWatchdog implements LoggerInterface
 {
 
     const LOGGER_EMERGENCY  = 0;
@@ -30,49 +29,15 @@ class LoggerWatchdog implements LoggerInterface, DebugLoggerInterface
     const LOGGER_DEBUG      = 7;
 
     /**
-     * @var array
-     */
-    protected $logs;
-
-    public function __construct()
-    {
-        $this->logs = array(
-            self::LOGGER_EMERGENCY  => array(),
-            self::LOGGER_ALERT      => array(),
-            self::LOGGER_CRITICAL   => array(),
-            self::LOGGER_ERROR      => array(),
-            self::LOGGER_WARNING    => array(),
-            self::LOGGER_NOTICE     => array(),
-            self::LOGGER_INFO       => array(),
-            self::LOGGER_DEBUG      => array()
-        );
-    }
-
-    /**
-     * Persist the log messages
-     */
-    public function __destruct()
-    {
-        foreach ($this->logs as $priority => $logs) {
-            foreach ($logs as $log) {
-                watchdog($log['context'], $log['message'], $log['variables'], $priority);
-            }
-        }
-    }
-
-    /**
      * @param $priority
      * @param $message
      * @param array $variables
      */
     public function log($priority, $message, array $variables = array())
     {
-        $this->logs[$priority][] = array(
-            'context'      => 'Symfony2',
-            'message'      => $message,
-            'priorityName' => $this->getPriorityName($priority),
-            'variables'    => $variables
-        );
+        if (function_exists('watchdog')) {
+            watchdog('Symfony2', $message, $variables, $priority);
+        }
     }
 
     /**
@@ -137,49 +102,5 @@ class LoggerWatchdog implements LoggerInterface, DebugLoggerInterface
     public function debug($message, array $context = array())
     {
         $this->log(self::LOGGER_DEBUG, $message, $context);
-    }
-
-    /**
-     * Returns an array of logs.
-     *
-     * A log is an array with the following mandatory keys:
-     * timestamp, message, priority, and priorityName.
-     * It can also have an optional context key containing an array.
-     *
-     * @return array An array of logs
-     */
-    public function getLogs()
-    {
-        $logs = array();
-
-        foreach ($this->logs as $logsByPriority) {
-            foreach ($logsByPriority as $log) {
-                $logs[] = $log;
-            }
-        }
-
-        return $logs;
-    }
-
-    /**
-     * Returns the number of errors.
-     *
-     * @return integer The number of errors
-     */
-    public function countErrors()
-    {
-        return count($this->logs[self::LOGGER_ERROR]) + count($this->logs[self::LOGGER_CRITICAL]);
-    }
-
-    /**
-     * Get the priority name
-     *
-     * @param  integer $priority The priority id
-     *
-     * @return string The priority name
-     */
-    private function getPriorityName($priority)
-    {
-        return ($priority != self::LOGGER_INFO && $priority != self::LOGGER_DEBUG) ? 'ERROR' : 'INFO';
     }
 }
